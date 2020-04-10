301c186b8ab9dcd55072df55bd65802488ad48a1
garahad <bfsudong@gmail.com>
2020-04-10 16:45:22 +0900

    가장 기본 resolver 세팅

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,28 +1,33 @@
-// import { User } from './entity/User';
+import { User } from './entity/User';
 
 // Provide resolver functions for your schema fields
 export const resolvers = {
   Query: {
-    getUser: async () => {
-      // const { id } = args;
-      return true;
-      // return await User.findOne({ where: { id } });
+    getUser: async (_: any, args: any) => {
+      const { id } = args;
+      const user = await User.findOne({ where: { id } });
+      return user;
     },
+    // getQuestions: () => {},
+    // getCategories: () => {},
   },
   Mutation: {
-    addUser: async () => {
-      // const { firstName, lastName, age } = args;
+    addUser: async (_: any, args: any) => {
+      const { snsId, provider, email, nick, password } = args;
       try {
-        // const user = User.create({
-        //   firstName,
-        //   lastName,
-        //   age
-        // });
+        const user = User.create({
+          snsId,
+          provider,
+          email,
+          nick,
+          password,
+        });
 
-        // await user.save();
+        await user.save();
 
         return true;
       } catch (error) {
+        console.log('error', error);
         return false;
       }
     },
