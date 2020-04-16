40c92b96751d295b33e5ca58e100973d7e3714af
garahad <bfsudong@gmail.com>
2020-04-16 13:30:37 +0900

    서버 보일러 플레이트 완성 (쿼리 받을 수 있게)
    기본적 cors랑 그에 따른 tsconfig 설정 변경

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,10 @@
 import 'reflect-metadata';
 import { createConnection } from 'typeorm';
-import * as express from 'express';
+import express from 'express';
 import { ApolloServer } from 'apollo-server-express';
+import cookieParser from 'cookie-parser';
+import morgan from 'morgan';
+import cors from 'cors';
 import { resolvers, schemas } from './graphql';
 
 // 나중에 HF server 보고 cors 설정 등 추가할 수
@@ -12,6 +15,16 @@ const startServer = async () => {
   await createConnection();
 
   const app = express();
+
+  const corsOption = {
+    credentials: true,
+    origin: true,
+  };
+  app.use(cors(corsOption));
+  app.use(morgan('dev'));
+  app.use(cookieParser());
+  app.use(express.json());
+  app.use(express.urlencoded({ extended: true }));
 
   server.applyMiddleware({ app });
 
